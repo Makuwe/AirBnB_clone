fd5b8d49b3880ad49d0aa0d61ea0bd9e1d92985d
Makuwe <mmakuwe98@gmail.com>
2024-05-18 20:23:39 -0700

    BaseModel file

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
